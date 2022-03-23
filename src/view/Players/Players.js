0b1589b80644f5d6718376f86bbcbf8c4b10773d
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-23 13:45:11 -0700

    added map for players

--- src/view/Players/Players.js
+++ src/view/Players/Players.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchPlayers } from '../../services/players';
 
 export default function Players() {
-  return <div>Players</div>;
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const data = await fetchPlayers();
+        setPlayers(data);
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    fetch();
+  }, []);
+  return (
+    <div>
+      Players:
+      <ul>
+        {players.map((player) => (
+          <li key={player.id}>
+            <Link to={`/players/${player.id}`}>{player.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
