0679d6af218dff9fa7cf8c516ff3a964a29981ba
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-23 12:08:42 -0700

    fetch call works

---
+++ src/services/teams.js
@@ -0,0 +1,11 @@
+import { checkError, client } from './client';
+
+export const fetchTeams = async () => {
+  const resp = await client.from('teams').select();
+  return checkError(resp);
+};
+
+export const fetchTeamsById = async (id) => {
+  const resp = await client.from('teams').select('*, players(*)').match({ id });
+  return checkError(resp);
+};
--- src/view/Teams/Teams.js
+++ src/view/Teams/Teams.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchTeams } from '../../services/teams';
 
 export default function Teams() {
+  const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const data = await fetchTeams();
+        console.log(data);
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    fetch();
+  }, []);
+
   return <div>Teams</div>;
 }
