f97e8e2b7f48a271c64a9fd60f5bc65c4ef410f5
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-23 11:48:13 -0700

    planned routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import PlayerDetail from './components/PlayerDetail/PlayerDetail';
+import TeamDetail from './components/TeamDetail/TeamDetail';
+import Header from './view/Header/Header';
+import Home from './view/Home/Home';
+import Players from './view/Players/Players';
+import Teams from './view/Teams/Teams';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App"></div>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path={'/players'}>
+          <Players />
+        </Route>
+        <Route exact path={'/teams'}>
+          <Teams />
+        </Route>
+        <Route path={'/teams/:id'}>
+          <PlayerDetail />
+        </Route>
+        <Route path={'/players/:id'}>
+          <TeamDetail />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
